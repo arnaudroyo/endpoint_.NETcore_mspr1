ec758aa9f7e31bf12c61afaf46ce72eee623b50b
Arnaud ROYO <arnaud.royo@epsi.fr>
2021-02-03 11:23:51 +0100

    seandriard.xyz

--- ENDPOINT/Program.cs
+++ ENDPOINT/Program.cs
@@ -15,7 +15,7 @@ namespace ENDPOINT
         static void Main(string[] args)
         {
             _listener = new HttpListener();
-            _listener.Prefixes.Add("http://217.69.0.125:80/");
+            _listener.Prefixes.Add("http://seandriard.xyz:80/");
             _listener.Start();
             _listener.BeginGetContext(OnContext, null);
             Console.WriteLine("Listening..");
