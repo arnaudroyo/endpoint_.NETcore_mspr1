9b111d96937ef76aeb1ee4bc41877c7fb0659b6c
Arnaud ROYO <arnaud.royo@epsi.fr>
2021-02-06 17:57:51 +0100

    *:420

--- ENDPOINT/Program.cs
+++ ENDPOINT/Program.cs
@@ -15,7 +15,7 @@ namespace ENDPOINT
         static void Main(string[] args)
         {
             _listener = new HttpListener();
-            _listener.Prefixes.Add("http://0.0.0.0:420/");
+            _listener.Prefixes.Add("http://*:420/");
             _listener.Start();
             _listener.BeginGetContext(OnContext, null);
             Console.WriteLine("Listening..");
